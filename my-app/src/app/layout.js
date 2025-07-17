ad7fc81b6674fef57c9ebf52dfac26c83b0e8bca
Pruthvi Gururaj <pruthvigururaj1998@gmail.com>
2025-07-17 13:21:08 +0530

    second commit

--- my-app/src/app/layout.js
+++ my-app/src/app/layout.js
@@ -20,6 +20,8 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <head>
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
         <link
           href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined"
           rel="stylesheet"
